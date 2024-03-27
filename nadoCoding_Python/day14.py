34245f2c4dc2d0df0a12832d3474a5aa5551ef5d
Yuuri <109029219+gayulz@users.noreply.github.com>
2024-03-27 23:03:27 +0900

    2024/03/27 Python Study

--- nadoCoding_Python/day14.py
+++ nadoCoding_Python/day14.py
@@ -70,3 +70,20 @@ class AttackUnit:
         self.damage = damage
         print(f"{self.name} 유닛이 생성되었습니다.")
         print(f"체력 : {self.hp} , 공격력 : {damage}")
+
+    def attack(self, location):
+        print(f"{self.name} : {location} 방향으로 적군을 공격합니다 [공격력 {self.damage}]")
+
+    def damaged(self, damage):
+        print(f"{self.name} : {damage}를 입었습니다.")
+        self.hp-=damage
+        print(f"{self.name} : {self.hp}")
+        if self.hp <= 0 :
+            print(f"{self.name} : 파괴되엇습니다")
+
+firebat1 = AttackUnit("파이어뱃", 50,60)
+firebat1.attack("5시")
+
+firebat1.damaged(25)
+firebat1.damaged(25)
+
